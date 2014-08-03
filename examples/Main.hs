b64c190793b8c87afb6dbccfb6e4f74451af1445
Kyle <hanooter@gmail.com>
2014-08-03 00:30:37 -0700

    Fix example

--- examples/Main.hs
+++ examples/Main.hs
@@ -127,7 +127,7 @@ main = do
       return (GlobalApp sess auth, RequestState Nothing)
   
   -- | Ability to easily run your handlers w/o a server.
-  hResult <- debugHandler opts $ handleSimple "Hello from console!"
+  hResult <- runRawHandler opts $ handleSimple "Hello from console!"
   either print (\r -> (showResponseBody r) >>= print) hResult
   
   -- | Or simply debug some stuff.
