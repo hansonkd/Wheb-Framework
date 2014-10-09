f8be1799df1c6245f1ae0be823d2269dd2b01a7d
Alexander Kjeldaas <ak@formalprivacy.com>
2014-10-09 13:11:48 +0200

    Removed explicit dependence on SessionApp for AuthApp
    Moved registration and account handling to an Account plugin.
    Added support for unregistering an account as well as explicit
    registration by email vs username and with or without password (less
    stringly typed).
    
    Added backend-specific data types for Claims, Credentials, Errors, and
    UserInfo with useful defaults, the defaults being:
    
    - default credentials supporting password, (soft)hostname, 2FA
    token, device token, and JWT tokens.
    - empty default for UserInfo
    - empty default for Claims
    - expanded set of errors based on implementing a real application backend.

--- wheb-base/src/Web/Wheb/Plugins/Debug/MemoryBackend.hs
+++ wheb-base/src/Web/Wheb/Plugins/Debug/MemoryBackend.hs
@@ -3,19 +3,21 @@ module Web.Wheb.Plugins.Debug.MemoryBackend where
 import Control.Concurrent.STM (atomically, modifyTVar, newTVarIO, readTVarIO, TVar, writeTVar)
 import Control.Monad (liftM)
 import Control.Monad.IO.Class (MonadIO(liftIO))
+import Control.Monad.Trans.Except (runExceptT, throwE)
 import Data.Map as M (alter, delete, empty, insert, lookup, Map, member, update)
 import Data.Maybe (fromMaybe)
 import Data.Text (Text)
 import Data.ByteString (ByteString)
 import Web.Wheb (InitM)
+import Web.Wheb.Plugins.Account
 import Web.Wheb.Plugins.Auth
 import Web.Wheb.Plugins.Cache
 import Web.Wheb.Plugins.Session
 
-data SessionData = SessionData 
+data SessionData = SessionData
   { sessionMemory ::  TVar (M.Map Text (M.Map Text Text)) }
 data UserData = UserData
-  { userStorage :: TVar (M.Map UserKey PwHash) }
+  { userStorage :: TVar (M.Map UserKey (Maybe PwHash)) }
 data CacheData = CacheData
   { cacheStorage :: TVar (M.Map Text ByteString) }
 
@@ -31,11 +33,11 @@ instance CacheBackend CacheData where
   backendCacheDelete key (CacheData tv) =
       liftIO $ atomically $ modifyTVar tv (M.delete key)
 
--- | In memory session backend. Session values 
+-- | In memory session backend. Session values
 -- will not persist after server restart.
 instance SessionBackend SessionData where
   backendSessionPut sessId key content (SessionData tv) =
-      let insertFunc = (\sess -> 
+      let insertFunc = (\sess ->
                           Just $ M.insert key content (fromMaybe M.empty sess)
                        )
           tVarFunc = M.alter insertFunc sessId
@@ -48,32 +50,47 @@ instance SessionBackend SessionData where
   backendSessionClear sessId (SessionData tv) =
       liftIO $ atomically $ modifyTVar tv (M.delete sessId)
 
--- | In memory auth backend. User values 
+instance AuthTypes UserData
+-- | In memory auth backend. User values
 -- will not persist after server restart.
 instance AuthBackend UserData where
-  backendGetUser name (UserData tv) = do
-        possUser <- liftM (M.lookup name) $ liftIO $ readTVarIO tv
+  backendGetUser ui@(UserId key _type) (UserData tv) = do
+        possUser <- liftM (M.lookup key) $ liftIO $ readTVarIO tv
         case possUser of
           Nothing -> return Nothing
-          Just _ -> return $ Just (AuthUser name)
-  backendLogin name pw (UserData tv) = do
+          Just _ -> return $ Just (AuthUser ui ())
+  backendLogin ui@(UserId key _type) claim (AuthCredentials (Just pw) _ _ _ _) (UserData tv) = do
         users <- liftIO $ readTVarIO $ tv
-        let possUser = M.lookup name users
-            passCheck = fmap (verifyPw pw) possUser
-        case passCheck of
-            Just True -> return (Right $ AuthUser $ name)
-            Just False -> return (Left InvalidPassword)
-            Nothing -> return (Left UserDoesNotExist)
-  backendRegister (AuthUser name) pw (UserData tv) = do
-        users <- liftIO $ readTVarIO $ tv
-        if M.member name users
-            then return (Left DuplicateUsername)
-            else do
-                pwHash <- makePwHash pw
-                liftIO $ atomically $ writeTVar tv (M.insert name pwHash users)
-                return (Right $ AuthUser name)
+        runExceptT $ do
+          maybePw <- maybe (throwE UserDoesNotExist) return $ M.lookup key users
+          if maybe True (verifyPw pw) maybePw
+             then return $ AuthUser ui claim
+             else throwE InvalidPassword
+  backendLogin _ _ _ _ = return $ Left AuthInternalError
   backendLogout _ =  getUserSessionKey >>= deleteSessionValue
-                
+
+instance AccountBackend UserData where
+  backendRegister ui@(UserId name _) () (AuthCredentials pw _host _totp _dev _tok) (UserData tv) = do
+    users <- liftIO $ readTVarIO $ tv
+    if M.member name users
+      then return $ Left DuplicateUsername
+      else do
+      maybePwHash <- maybe (return Nothing) (\p -> return . Just =<< makePwHash p) pw
+      liftIO $ atomically $ writeTVar tv (M.insert name maybePwHash users)
+      return $ Right ui
+  backendRegisterByUsernamePassword un pw = backendRegister (UserId un $ Just UserName) () (credsByPw pw)
+  backendRegisterByEmailPassword em pw = backendRegister (UserId em $ Just EmailKey) () (credsByPw pw)
+  backendRegisterByEmail em               = backendRegister (UserId em $ Just EmailKey) () emptyCreds
+  backendUnregister (UserId name _) (UserData tv) = do
+    users <- liftIO $ readTVarIO $ tv
+    if not $ M.member name users
+       then return $ Left AuthRegUserDoesNotExist
+       else do
+      liftIO $ atomically $ writeTVar tv (M.delete name users)
+      return $ Right ()
+
+
+
 initSessionMemory :: InitM g s m SessionContainer
 initSessionMemory = do
   tv <- liftIO $ newTVarIO $ M.empty
@@ -83,7 +100,7 @@ initAuthMemory :: InitM g s m AuthContainer
 initAuthMemory = do
   tv <- liftIO $ newTVarIO $ M.empty
   return $! AuthContainer $ UserData tv
-  
+
 initCacheMemory :: InitM g s m CacheContainer
 initCacheMemory = do
   tv <- liftIO $ newTVarIO $ M.empty
